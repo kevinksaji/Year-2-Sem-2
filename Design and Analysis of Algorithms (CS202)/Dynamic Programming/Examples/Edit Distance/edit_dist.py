b2bc76c1c47c526359734e44578fc2d62008e3d9
kevinksaji <kevink.saji.2022@scis.smu.edu.sg>
2024-02-28 16:58:55 +0800

    completed LCS.py and edit_dist.py

--- Design and Analysis of Algorithms (CS202)/Dynamic Programming/Examples/Edit Distance/edit_dist.py
+++ Design and Analysis of Algorithms (CS202)/Dynamic Programming/Examples/Edit Distance/edit_dist.py
@@ -1,18 +1,24 @@
 def edit_dist(s1, s2):
     # implement this function
     # the expected output for the inputs below are 2, 2, 3 respectively
-    n, m = len(s1), len(s2)
-    dist = [[0] * (m+1) for _ in range(n+1)]
+    n, m = len(s1), len(s2) # get the length of the two strings
+    dist = [[0] * (m+1) for _ in range(n+1)] # create a 2D array of size (n+1) x (m+1)
+
     for i in range(n+1):
-        dist[i][m] = n - i
+        dist[i][m] = n - i # fill the last column with the difference between the length of the two strings
     for j in range(m+1):
-        dist[n][j] = m - j
-    for i in range(n-1, -1, -1):
-        for j in range(m-1, -1, -1):
-            dist[i][j]  = min(dist[i+1][j+1] + (s1[i] != s2[j]), 
-                              dist[i+1][j] + 1, # the cell below
-                              dist[i][j+1] + 1) # the cell to the right
-    
+        dist[n][j] = m - j # fill the last row with the difference between the length of the two strings
+
+    for i in range(n-1, -1, -1): # start from the second last row, 2nd parameter means stop at -1, 3rd parameter means step by -1
+
+        for j in range(m-1, -1, -1): # start from the second last column, 2nd parameter means stop at -1, 3rd parameter means step by -1
+            if s1[i] != s2[j]:
+                dist[i][j] = min(dist[i+1][j] + 1, dist[i][j+1] + 1, dist[i+1][j+1] + 1) # if the two elements are not the same, take the minimum of the three values (bottom + 1, right + 1, bottom right + 1)
+            else:
+                dist[i][j] = min(dist[i+1][j] + 1, dist[i][j+1] + 1, dist[i+1][j+1]) # if the two elements are the same, take the minimum of the three values(bottom + 1, right + 1, bottom right + 0)
+
+            # alternate to the if else statements
+                # dist[i][j] = min(dist[i+1][j] + 1, dist[i][j+1] + 1, dist[i+1][j+1]) + (s1[i] != s2[j])
     return dist[0][0]
 
 print(edit_dist('diary', 'binary'))
--- Design and Analysis of Algorithms (CS202)/Dynamic Programming/Examples/Longest Common Sequence/LCS.py
+++ Design and Analysis of Algorithms (CS202)/Dynamic Programming/Examples/Longest Common Sequence/LCS.py
@@ -6,21 +6,23 @@ B = ['park', 'his', 'car', 'before', 'watch', 'play']
 
 m, n = len(A), len(B)
 
-LCS = [[None] * (n+1) for _ in range(m+1)]
+LCS = [[0] * (n+1) for _ in range(m+1)]
 
 # write your code here
 # the expected output for the above input is 2.
 
-for i in range(n+1):
+for i in range(n+1): #first row are all 0
     LCS[i][0] = 0
-for j in range(m+1):
+
+for j in range(m+1): #first column are all 0
     LCS[0][j] = 0
 
+# start from 1 because the first row and column are already filled with 0 (base case)
 for i in range(1, m+1):
     for j in range(1, n+1): # build LCS[i][j]
-        if A[i-1] == B[j-1]:
-            LCS[i][j] = LCS[i-1][j-1] + 1
+        if A[i-1] == B[j-1]: # if the two elements are the same
+            LCS[i][j] = LCS[i-1][j-1] + 1 # add 1 to the previous LCS (add 1 to the value in the top left cell of the current cell)
         else:
-            LCS[i][j] = max(LCS[i-1][j], LCS[i][j-1])
+            LCS[i][j] = max(LCS[i-1][j], LCS[i][j-1]) # take the maximum of the previous LCS (max of the cell above and the cell beside)
 
 print(LCS[m][n])
