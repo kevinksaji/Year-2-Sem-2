43178d18e0399648a83e3b55f7feb180e31e5c5f
kevinksaji <kevink.saji.2022@scis.smu.edu.sg>
2024-02-28 15:35:08 +0800

    completed coin_change_limited.py

--- Design and Analysis of Algorithms (CS202)/Dynamic Programming/Extra Exercises/coin_change_limited.py
+++ Design and Analysis of Algorithms (CS202)/Dynamic Programming/Extra Exercises/coin_change_limited.py
@@ -3,3 +3,18 @@ denom = [(1, 2), (2, 2), (5, 2), (10, 2), (20, 1), (50, 1), (100, 2)]
 
 m = len(denom)
 n = 300
+
+denom.insert(0, 0) # insert a dummy denomination at the beginning of the list to make the index of the list the same as the index of the denominations
+
+min_coin = [[float('inf')]*(n+1) for i in range(m+1)]  # min_coin[i][j] is the minimum number of coins to make up j cents using the first i denominations
+
+for i in range(m+1):
+    min_coin[i][0] = 0 # base case: 0 coins to make up 0 cents
+
+for j in range(1, m+1): # j is the denomination we consider
+    for i in range(1, n+1): # i is the amount of cents we want to make up
+        for k in range(denom[j][1]+1): # k is the number of coins of the current denomination we use
+            if i >= k*denom[j][0]: # if i is greater than or equal to k*denom[j], the current denomination can be used to make up i cents
+                min_coin[j][i] = min(min_coin[j][i], k + min_coin[j-1][i-k*denom[j][0]]) # we take the minimum of the current minimum number of coins to make up i cents and k + the minimum number of coins to make up i-k*denom[j] cents
+
+print(min_coin[m][n])
